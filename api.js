0fae0bb4f577ff88d553e15ba0774f9f08d06c79
massimilianofronza <maxfronza97@gmail.com>
2018-10-26 16:17:06 +0200

    mio primo commit

--- api.js
+++ api.js
@@ -2,6 +2,6 @@ const express = require('express')
 const app = express()
 const PORT = process.env.PORT || 3000
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => res.send('Hello Word!'))
 app.listen(PORT, () => console.log('Example app listening on port'+ PORT))
 
