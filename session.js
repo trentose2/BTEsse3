8efb001f1697f3b0d49f57db5a72bb74b4061d07
xeela <michela.comperini@studenti.unitn.it>
2018-11-27 22:02:10 +0100

    Create session.js

---
+++ session.js
@@ -0,0 +1,67 @@
+const session_offered  = require ('/.session_offered')
+//POST, /sessionExams
+function create_session(idSession, exams){
+		const index = session_offered.findIndex((item) => {return item.id===idSession})
+		if (index!==-1 || exam == null) {
+			return 404
+		} else {
+			session_offered[index] ={idSession, exams}
+    	//controllo se c'è la nuova sessione
+	    	const index = session_offered.findIndex((item) => {return item.id===idSession})
+	    	if (index!==-1) {
+		var count = 0
+		for(i=0;i<session_offered.lenght;i++) { //controllo che sia unica
+			if (session_offered[l].id===idSession)
+			count++
+    		}
+    		if (count = 1) {
+		return 200
+    		}
+	 }
+	}
+}
+ 
+ //DELETE, /sessionExam/{idSession}
+function delete_session(idSession){
+	const index = session_offered.findIndex((item) => {return item.id===idSession})
+		if (index===-1) {
+			return 404
+		} else {
+			session_offered.splice(index,1)
+			return 200
+//GET, /sessionExams
+function get_sessions() {
+    if (session_offered !== null) {
+    	return session_offered
+    } else {
+	return 404   	
+}}
+	
+//GET,/sessionExams/{idSession} 
+function get_session_by_id (idSession) {
+ 	const index = session_offered.findIndex((item) => {return item.id===idSession})
+		if (index!==-1) {
+			return session_offered[index]
+		    }else {
+			 return 404
+			
+		    }
+		}
+}
+  
+//PUT,/sessionExams 
+function put_session(idSession, exams) {
+	const index = session_offered.findIndex((item) => {return item.id===idSession})
+	if (index!==-1) {
+		session_offered[index] ={idSession, exams}
+		return 200
+	} else {
+		return 404
+	}			
+}
+			
+module export = {put_session,
+		get_session_by_id,
+		get_sessions,
+		delete_session,
+		create_session}
