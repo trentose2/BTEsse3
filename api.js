12b7f00ca84d968f30343a5bfff169a73289e18f
xeela <michela.comperini@studenti.unitn.it>
2018-11-27 21:32:59 +0100

    Update api.js

--- api.js
+++ api.js
@@ -1,7 +1,103 @@
 const express = require('express')
+var bodyParser = require('body-parser')
+
 const app = express()
+app.use( bodyParser.json() )
+app.use(bodyParser.urlencoded({ extended: true }));
+
 const PORT = process.env.PORT || 3000
 
-app.get('/', (req, res) => res.send('Hello Word!'))
-app.listen(PORT, () => console.log('Example app listening on port'+ PORT))
+const SOME_NUM = process.env.def || 40
 
+var session_offered = [{idSession:23, exams: [{nameExam:'prova1', idExam:74},{nameExam:'prova2',idExam:12}]},
+                       {idSession:28, exams: [{nameExam:'prova2', idExam:74},{nameExam:'prova3',idExam:91}]}]
+   
+
+
+//POST, sessionExam
+app.post('/sessionExams', (req, res) => {
+    const new_idSession = req.body.idSession
+    const new_exams= req.body.exams
+    const new_session = [{idSession:91, exams: [{nameExam:prova5, idExam:47},{nameExam:prova6,idExam:22}]}]
+    session_offered.push(new_session)
+    //controllo se c'è la nuova sessione
+    const index = session_offered.findIndex((item) => {return item.id===req.params.idSession})
+    if (index===1) {
+        res.sendstatus(200)
+        res.json(new_session)
+        console.log(session_offered)
+		console.log('done')
+	}else {
+        res.status(404)
+	}
+ })
+ //DELETE, /sessionExam/{idSession}
+ app.delete('/sessionExams/:idSession', (req, res) => {
+    const index = session_offered.findIndex((item) => {return item.id===req.params.idSession})
+    if (index===-1) {
+        res.sendstatus(404)
+    } else {
+	var count = 0
+    	for(i=0;i<session_offered.lenght;i++) { //controllo che sia unico
+      		if (session_offered[l].id===req.params.idSession)
+          	count++
+    }
+    if (count >1) {
+      res.sendStatus(400)
+    } else {
+      session_offered.splice(index,1)
+      console.log('\ndeleting ',req.params.idSession)
+      console.log('now:',session_offered)
+      res.sendStatus(200) 
+ }}})
+
+//GET, sessionExams
+app.get('/sessionExams', (req, res) => {
+    res.json(session_offered)
+    res.sendStatus(200)
+});
+
+//GET,/sessionExams/{idSession}
+app.get('/sessionExams/:idSession', (req,res) => {
+      const index = session_offered.findIndex((item) => {return item.id===req.params.idSession})
+      if (index ===-1) {
+	      res.sendStatus(404)
+      } else {
+      var c = session_offered.idSession
+      if (index===-1) {
+          res.sendstatus(404)
+      }else {
+	for(i=0;i<c.exams.lenght;i++) {
+        	res.json(c.exams)
+	}
+        res.sendStatus(200)
+}}});
+
+app.put('/sessionExams/{idSession}', (req, res) => {
+    var b = parseInt('idSession',10)
+    if (b <= 0) {
+     res.sendStatus(400)
+   } else { 
+    //controllo se c'è la sessione
+    const index = session_offered.findIndex((item) => {return item.id===req.params.idSession})
+    if (index===-1) {
+	    res.sendStatus(404)
+    } else {
+	var count = 0
+    	for(i=0;i<session_offered.lenght;i++) { //controllo che sia unico
+      		if (session_offered[l].id===req.params.idSession)
+          	count++
+    }
+    if (count >1) {
+	res.sendStatus(400)
+    } else {
+	const old_idSession = req.body.idSession
+    	const old_exams = req.body.exams
+	const old_session = [{old_idSession,old_exams}] 
+	session_offered.push(old_session)
+        res.sendStatus(200)
+        res.json(old_session)
+	console.log(session_offered)
+}}}})
+
+module.exports  =  session_offered
