ef0b0f49ddb0505fa814909a4e2012ae3f1493a5
xeela <michela.comperini@studenti.unitn.it>
2018-11-27 22:00:29 +0100

    Create data.js

---
+++ data.js
@@ -0,0 +1,2 @@
+var session_offered = [{idSession:23, exams: [{nameExam:'prova1', idExam:74},{nameExam:'prova2',idExam:12}]},
+                       {idSession:28, exams: [{nameExam:'prova2', idExam:74},{nameExam:'prova3',idExam:91}]}]
