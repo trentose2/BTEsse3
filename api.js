c54c34f3d49b6b9751831b3c0d0d00738488b97a
Stefano Sega <stefano.sega-1@studenti.unitn.it>
2018-12-03 15:48:55 +0100

    Added APIs and tests to submissions and separated submissions from exams

--- api.js
+++ api.js
@@ -4,7 +4,10 @@ var fs = require("fs");
 const PORT = process.env.PORT || 3000;
 
 var exams = require('./resources/exams');
+var submissions = require('./resources/submissions');
 
+app.get('/', (req, res) => res.send('Welcome to BTESSE3'));
 app.use('/', exams);
+app.use('/', submissions);
 
 app.listen(PORT, () => console.log('Example app listening on port' + PORT));
